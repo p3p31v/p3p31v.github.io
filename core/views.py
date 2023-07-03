27e050fbbd7e47c2ec37ab22c1417ca4914d4395
jose <p3p31v@github.com>
2023-07-03 22:00:12 +0200

    Links added

--- core/views.py
+++ core/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render, HttpResponse
 
+html_base =""" 
+<h1>Mi web personal</h1>
+<ul>
+    <li><a href='/'>Portada</a></li>
+    <li><a href='/about-me'>Acerca de</a></li>
+
+</ul>
+"""
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Mi web personal</h1><h2>Portada</h2>')
+    return HttpResponse(html_base + """<h2>Portada</h2>""")
+def about(request):
+    return HttpResponse(html_base + """<h2>Acerca de</h2><p>Me llamo Joselito</p>""")
