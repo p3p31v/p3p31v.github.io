0c2a4ab6d568a8422221ed6850e3273d7a6356d6
jose <p3p31v@github.com>
2023-07-04 15:03:10 +0200

    New pages added for portfolio and contact

--- core/views.py
+++ core/views.py
@@ -5,12 +5,31 @@ html_base ="""
 <ul>
     <li><a href='/'>Portada</a></li>
     <li><a href='/about-me'>Acerca de</a></li>
+    <li><a href='/portfolio'>Portafolio</a></li>
+    <li><a href='/contact'>Contacto</a></li>
 
 </ul>
 """
 
 # Create your views here.
 def home(request):
-    return HttpResponse(html_base + """<h2>Portada</h2>""")
+    return HttpResponse(html_base + """
+    <h2>Portada</h2>
+    <p>Esta es la portada</p>
+    """)
+
 def about(request):
-    return HttpResponse(html_base + """<h2>Acerca de</h2><p>Me llamo Joselito</p>""")
+    return HttpResponse(html_base + """
+    <h2>Acerca de</h2>
+    <p>Me llamo Joselito</p>
+    """)
+def portfolio(request):
+    return HttpResponse(html_base + """
+    <h2>Portafolio</h2>
+    <p>Algunos de mis trabajos</p>
+    """)
+
+def contact(request):
+    return HttpResponse(html_base + """
+    <h2>Contacto</h2><p>Mi E-mail: <a href="hola@hola.com">Mi e-mail</a></p>
+    """)
