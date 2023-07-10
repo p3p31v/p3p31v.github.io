c64d843ab761104441632c34399cc210b83e0c72
jose <p3p31v@github.com>
2023-07-10 15:49:31 +0200

    Everything made with renders

--- core/views.py
+++ core/views.py
@@ -1,32 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
-html_base =""" 
-<h1>Mi web personal</h1>
-<ul>
-    <li><a href='/'>Portada</a></li>
-    <li><a href='/about-me'>Acerca de</a></li>
-    <li><a href='/portfolio'>Portafolio</a></li>
-    <li><a href='/contact'>Contacto</a></li>
-
-</ul>
-"""
 
 # Create your views here.
 def home(request):
     return render(request,"core/home.html")
-
 def about(request):
-    return HttpResponse(html_base + """
-    <h2>Acerca de</h2>
-    <p>Me llamo Joselito</p>
-    """)
+    return render(request,"core/about.html")
 def portfolio(request):
-    return HttpResponse(html_base + """
-    <h2>Portafolio</h2>
-    <p>Algunos de mis trabajos</p>
-    """)
-
+    return render(request,"core/portfolio.html")
 def contact(request):
-    return HttpResponse(html_base + """
-    <h2>Contacto</h2><p>Mi E-mail: <a href="hola@hola.com">Mi e-mail</a></p>
-    """)
+    return render(request,"core/contact.html")
